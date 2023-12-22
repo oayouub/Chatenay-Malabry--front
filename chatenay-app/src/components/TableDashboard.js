fbc7089ca3950f17dfd3009178c67677c344b4ce
ayoub ouahid <aouahid123@gmail.com>
2023-12-22 15:45:14 +0100

    fix table size

--- chatenay-app/src/components/TableDashboard.js
+++ chatenay-app/src/components/TableDashboard.js
@@ -1,28 +1,13 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import { Link } from "react-router-dom";
-import supabase from "../server/supabase"
-import {useState, useEffect } from 'react';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import supabase from "../server/supabase";
 
 
 
@@ -42,10 +27,10 @@ export default function BasicTable() {
     }
     getPosts();
     console.log('posts2', posts)
-  
   }, []);
+
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} sx={{width: '1200px'}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
